3d8e41160f5f1664e6bfa4a3a7b1011ab7c2454f
Mariana Lima <mariana.lima118@outlook.com>
2024-10-29 20:43:27 -0300

    incluido cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import routes from "./routes.js";
+import cors from 'cors'
 
 const app = express();
 const port = 3001;
 
+app.use(cors())
 app.use(express.json())
 app.use('/', routes)
 
